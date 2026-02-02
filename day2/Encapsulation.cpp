ca0bde28cff68b404ac8385e6fcfca3829c6571c
Rajesh Patidar <mp2006011@gmail.com>
2026-02-02 18:58:43 +0530

    Add files via upload

---
+++ day2/Encapsulation.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class SportsCar {
+private:
+    string brand;
+    string model;
+    bool isEngineOn;
+    int currentSpeed;
+    int currentGear;
+
+    // new variable to explain setters
+    string tyreCompany;
+ 
+public:
+    SportsCar(string b, string m) {
+        this->brand = b;
+        this->model = m;
+        isEngineOn = false;
+        currentSpeed = 0;
+        currentGear = 0;
+        tyreCompany = "MRF";
+    }
+
+    int getSpeed() {
+        return currentSpeed;
+    }
+
+    string getTyreCompany() {
+        return tyreCompany;
+    }
+
+    void setTyreCompany(string tyreCompany) {
+        this->tyreCompany = tyreCompany;
+    }
+
+    void startEngine() {
+        isEngineOn = true;
+        cout << brand << " " << model << " : Engine starts with a roar!" << endl;
+    }
+
+    void shiftGear(int gear) {
+        if (!isEngineOn) {
+            cout << brand << " " << model << " : Engine is off! Cannot Shift Gear." << endl;
+            return;
+        }
+        currentGear = gear;
+        cout << brand << " " << model << " : Shifted to gear " << currentGear << endl;
+    }
+
+    void accelerate() {
+        if (!isEngineOn) {
+            cout << brand << " " << model << " : Engine is off! Cannot accelerate." << endl;
+            return;
+        }
+        currentSpeed += 20;
+        cout << brand << " " << model << " : Accelerating to " << currentSpeed << " km/h" << endl;
+    }
+
+    void brake() {
+        currentSpeed -= 20;
+        if (currentSpeed < 0) currentSpeed = 0;
+        cout << brand << " " << model << " : Braking! Speed is now " << currentSpeed << " km/h" << endl;
+    }
+
+    void stopEngine() {
+        isEngineOn = false;
+        currentGear = 0;
+        currentSpeed = 0;
+        cout << brand << " " << model << " : Engine turned off." << endl;
+    }
+
+    ~SportsCar() {}
+};
+
+// Main Method
+int main() {
+
+    SportsCar* mySportsCar = new SportsCar("Ford", "Mustang");
+
+    mySportsCar->startEngine();
+    mySportsCar->shiftGear(1);
+    mySportsCar->accelerate();
+    mySportsCar->shiftGear(2);
+    mySportsCar->accelerate();
+    mySportsCar->brake();
+    mySportsCar->stopEngine();
+
+    // //Setting arbitrary value to speed. we dont need access outside class to change it just need value
+    // mySportsCar->currentSpeed = 500;
+
+    // cout << "Current Speed of My Sports Car is set to " << mySportsCar->currentSpeed << endl;
+
+    cout << "Current Speed of My Sports Car is " << mySportsCar->getSpeed() << endl;
+
+    delete mySportsCar;    
+
+    return 0;
+}
